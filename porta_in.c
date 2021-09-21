5988dd9231524d2f20918335d297317a00a239b8
josephdamato99 <60833991+josephdamato99@users.noreply.github.com>
2021-09-21 13:52:49 -0400

    Add files via upload

---
+++ porta_in.c
@@ -0,0 +1,16 @@
+#include <xc.h>
+#include <inttypes.h>
+#include "porta_in.h"
+
+//low level code to initialize port
+void porta_in_init(){
+    ANSELA = 0; //digital
+    TRISA = 0x1F; //input
+    CNPUA = 0x1F; //internal pull up resistor
+}
+
+uint8_t porta_in_read(){
+    uint8_t data;
+    data = PORTA;
+    return data;
+}
