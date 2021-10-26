65fbd5efcc4e15f41c88b3f02fd0b3404a0c3f00
allytheengineer <78810943+allytheengineer@users.noreply.github.com>
2021-10-26 11:21:01 -0400

    Update ts_ldc.c

--- lab5.X/ts_ldc.c
+++ lab5.X/ts_ldc.c
@@ -24,8 +24,9 @@ uint8_t ts_lcd_get_ts(uint16_t *x, uint16_t *y){
   p.x = 0;
   p.y = 0;
   getPoint(&p);
-  *x = 320-((float)(p.y-130) / (800.0f)) * (320.0f);
-  *y = ((float)(p.x - 180) / (680.0f)) * (240.0f);
+  //interpolation
+  *x = 320-((float)(p.y-130)/ 800.0f)) *(320.0f);
+  *y = ((float)(p.x-180)/(680.0f))*(240.0f);
 }
 
 //checks if screen is pressed
@@ -34,28 +35,27 @@ uint8_t screenPress(){
   p.x = 0;
   p.y = 0;
   getPoint(&p);
-  return (p.z > 0);
+  return(p.z>0);
 }
 
 //touch makes cross hair
 void test_touch(){
   static char buffer[30];
-  static uint16_t displayX, displayY;
-
+  static uint16_t newX, newY;
   if(screenPress()){
-    tft_drawLine(displayY-10, displayX,displayY+10, displayX,0x0000);
-    tft_drawLine(displayY, displayX+10, displayY, displayX - 10, 0x0000);
-    ts_lcd_get_ts(&displayY, &displayX);
+    tft_drawLine(newY-15, newX,newY+15, newX,0x0000);
+    tft_drawLine(newY, newX+15, newY, newX-15, 0x0000);
+    ts_lcd_get_ts(&newY, &newX);
     tft_setCursor(10, 10);
-    tft_setTextSize(2);
+    tft_setTextSize(4);
     tft_setTextColor(ILI9341_BLACK);
     tft_writeString(buffer);
     tft_setCursor(10, 10);
     tft_setTextColor(ILI9341_WHITE);
-    sprintf(buffer,"x: %d, y: %d", displayX, displayY);
+    sprintf(buffer,"x: %d, y: %d", newX,newY);
     tft_writeString(buffer);
-    tft_drawLine(displayY - 10, displayX,      displayY + 10, displayX,      0xffff);
-    tft_drawLine(displayY,      displayX + 10, displayY,      displayX - 10, 0xffff);
+    tft_drawLine(newY-15, newX,newY+15, newX,0xffff);
+    tft_drawLine(newY,newX+15, newY,newX-15, 0xffff);
   }
 }
 
